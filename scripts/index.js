1c0b566d18dce0aa60f749679f9adf4ade293d0e
Dmitry Pertsev <disay2040@yandex.ru>
2024-03-05 23:26:33 +0300

    На проверку 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const places = document.querySelector('.places__list');
+ 
+function createCard({name, link}, deleteCallBack) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCallBack);
+    return cardElement;
+};
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function removeElement(evt) {
+    evt.target.parentElement.remove();
+};
+    
+for (let i=0; i< initialCards.length; i++) {
+    places.append(createCard(initialCards[i], removeElement));
+} ;
